37beae562ecf7f911eabb59aa1180e717a8b0b6c
Nikita Shaw <nikita@topfreight.ai>
2023-06-02 12:58:50 +0530

    feat : resume-builder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./components/Home";
+import Resume from "./components/Resume";
 import './App.css';
 
 function App() {
+  const [result, setResult] = useState({});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home setResult={setResult} />} />
+          <Route path="./resume" element={<Resume result={result} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
